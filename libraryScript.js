7dcedcf04fa72d0e5758e0ed6c985f3ff1de99b2
Doug Hughes <dhughes8906@gmail.com>
2019-12-21 13:15:50 -0500

    Adds full functionality for the add book window. Books can now be added to the library through the use of the window.

--- libraryScript.js
+++ libraryScript.js
@@ -82,8 +82,15 @@ const bookShelf = document.querySelector("#bookShelf");
 bookLibrary.push(new Book("The Hobbit", "J.R.R. Tolkien", 562, true));
 bookLibrary.push(new Book("Crime and Punishment", "Fyodor Dostoevsky",
 	427, true));
+bookLibrary.push(new Book("The Great Gatsby", "F. Scott Fitzgerald",
+	189, false));
+bookLibrary.push(new Book("The Odyssey", "Homer", 715, true));
+bookLibrary.push(new Book("Catch-22", "Joseph Heller", 311, false));
 addToBookshelf(bookLibrary[0]);
 addToBookshelf(bookLibrary[1]);
+addToBookshelf(bookLibrary[2]);
+addToBookshelf(bookLibrary[3]);
+addToBookshelf(bookLibrary[4]);
 
 // reference to the modal content that is displayed when the
 // user wants to add a book or to view the full details of
@@ -111,9 +118,27 @@ const addClose = document.querySelector("#addClose");
 // reference to body element
 const body = document.querySelector("body");
 
+// button used to submit a new book
+const newBookBtn = document.querySelector("#submitBook");
+
+// each of the input fields for the window that allows the user
+// to add a book
+const titleInput = document.querySelector("#titleInput");
+const authorInput = document.querySelector("#authorInput");
+const pagesInput = document.querySelector("#pagesInput");
+const finishedInput = document.querySelector("#hasRead");
+const unfinishedInput = document.querySelector("#notRead");
+
+// the error area for the window that allows the user to add a book
+const errorArea = document.querySelector("#errorArea");
+
+// holds true if the add book window is currently open
+let addOpen = false;
+
 // bring up the modal content to add a book to the library when the
 // add button is clicked
 addBtn.addEventListener("click", function() {
+	addOpen = true;
 	modalCont.style.display = "block";
 	addBox.style.display = "block";
 	// get the current position of the scroll, so the position of 
@@ -129,6 +154,11 @@ addBtn.addEventListener("click", function() {
 // close the modal when the screen is clicked (i.e. outside of the modal
 // box)
 modalCont.addEventListener("click", function() {
+	if (addOpen) {	
+		clearAddFields();
+		clearErrors();
+		addOpen = false;
+	}
 	modalCont.style.display = "none";
 	addBox.style.display = "none";
 	bookViewBox.style.display = "none";
@@ -137,10 +167,18 @@ modalCont.addEventListener("click", function() {
 
 // close the modal when the add box close button is clicked
 addClose.addEventListener("click", function() {
+	closeAddWindow();	
+});
+
+// close the add book window and clear all inputs / errors
+function closeAddWindow() {
+	clearAddFields();
+	clearErrors();
+	addOpen = false;
 	modalCont.style.display = "none";
 	addBox.style.display = "none";
 	body.style.overflow = "visible";
-});
+}
 
 // prevent clicks on the add box itself from closing out the modal 
 // content
@@ -148,4 +186,80 @@ addBox.addEventListener("click", function(event) {
 	event.stopPropagation();	
 });
 
+// error messages that are used for the add book window
+const emptyError = document.createElement("P");
+emptyError.textContent = "Error: fill in any empty field(s).";
+const pagesError = document.createElement("P");
+pagesError.textContent = "Error: # of pages needs to be a postive number" +
+	" without any commas and without a decimal.";
 
+// adds a book with the given information, if all of the information
+// in the window is filled out (and valid). Otherwise prints error
+// message(s) to the window
+newBookBtn.addEventListener("click", function() {
+	// clear out any error messages from the last attempt
+	clearErrors();
+	// check if new inputs have any errors
+	let hasErrors	= checkErrors();
+	
+	// no errors, so add the book and exit the window
+	if (!hasErrors) {
+		// add the new book to the library	
+		bookLibrary.push(new Book(titleInput.value, authorInput.value, 
+			+(pagesInput.value), finishedInput.checked));
+		addToBookshelf(bookLibrary[bookLibrary.length - 1]);
+		// close the window
+		closeAddWindow();
+	}
+});
+
+// clears all error messages from the add book window
+function clearErrors() {
+	while(errorArea.lastChild) {
+		errorArea.removeChild(errorArea.lastChild);
+	}	
+}
+
+// checks if there are any errors in the inputs for the add book
+// window. If there are, the relevant error messages are output to
+// the window and true is returned. If there are no errors, false
+// is returned.
+
+function checkErrors() {
+	let hasError = false;
+	// make sure all of the fields are not blank	
+	let newTitle = titleInput.value;
+	let newAuthor = authorInput.value;
+	let numPages = pagesInput.value;
+	let hasRead = (finishedInput.checked) ? true : -1;
+	if (hasRead === -1) {
+		hasRead = (unfinishedInput.checked) ? false : -1;
+	}
+
+	if (newTitle === "" || newAuthor === "" || numPages === "" || 
+		hasRead === -1) {
+		hasError = true;
+		errorArea.appendChild(emptyError);
+	}	
+
+	if (numPages !== "") {
+		// make sure a positive integer was entered for # of pages
+		numPages = +numPages;
+
+		if (isNaN(numPages) || !(Number.isInteger(numPages)) || numPages <= 0) {
+			hasError = true;
+			errorArea.appendChild(pagesError);
+		}
+	}	
+
+	return hasError;	
+}
+
+// clears all of the fields in the add book window
+function clearAddFields() {
+	titleInput.value = "";
+	authorInput.value = "";
+	pagesInput.value = "";
+	finishedInput.checked = false;
+	unfinishedInput.checked = false;
+}
